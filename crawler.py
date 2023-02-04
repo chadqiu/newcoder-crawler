7076bded80c162c4598f309fae6994ab2902c11e
chadqiu <chadqiu@163.com>
2023-02-04 20:35:26 +0800

    optimize code style

--- crawler.py
+++ crawler.py
@@ -7,62 +7,68 @@ import pymysql
 import smtplib
 from email.mime.text import MIMEText
 
-def get_newcoder_page(page = 1, keyword = "补录"):
-    header = {
-        "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
-        "content-type":"application/json"
-    }
-    data = {
-        "type": "all", 
-        "query": keyword, 
-        "page": page, 
-        "tag": [], 
-        "order": "create"
-    }
-    x = requests.post('https://gw-c.nowcoder.com/api/sparta/pc/search', data = json.dumps(data), headers = header, )
-    return x.json()
 
-def parse_newcoder_page(data, skip_words = [], start_date = '2023'):
+def _parse_newcoder_page(data, skip_words, start_date):
     assert data['success'] == True
-    pattern = re.compile("|".join(skip_words)) 
+    pattern = re.compile("|".join(skip_words))
     res = []
     for x in data['data']['records']:
         x = x['data']
         dic = {"user": x['userBrief']['nickname']}
-        
+
         x = x['contentData'] if 'contentData' in x else x['momentData']
         dic['title'] = x['title']
         dic['content'] = x['content']
         dic['id'] = int(x['id'])
         dic['url'] = 'https://www.nowcoder.com/discuss/' + str(x['id'])
-        
-        if len(skip_words) > 0 and pattern.search(x['title'] + x['content']) != None:
+
+        if len(skip_words) > 0 and pattern.search(x['title'] + x['content']) != None:  # 关键词正则过滤
             continue
-  
+
         createdTime = x['createdAt'] if 'createdAt' in x else x['createTime']
         dic['createTime'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(createdTime // 1000))
         dic['editTime'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(x['editTime'] // 1000))
-        
-        if dic['editTime'] < start_date: continue
+
+        if dic['editTime'] < start_date:  # 根据时间过滤
+            continue
         res.append(dic)
-        
+
     return res
 
-def upsert_to_db(data, host, user, passwd, database,charset, port):
+
+def get_newcoder_page(page=1, keyword="校招", skip_words=[], start_date='2023'):
+    header = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+        "content-type": "application/json"
+    }
+    data = {
+        "type": "all",
+        "query": keyword,
+        "page": page,
+        "tag": [],
+        "order": "create"
+    }
+    x = requests.post('https://gw-c.nowcoder.com/api/sparta/pc/search', data=json.dumps(data), headers=header, )
+    data = _parse_newcoder_page(x.json(), skip_words, start_date)
+    return data
+
+
+def upsert_to_db(data, host, user, passwd, database, charset, port):
     db = pymysql.connect(
-          host=host,
-          user=user,
-          passwd=passwd,
-          database = database,
-          charset=charset,
-          port = port
+        host=host,
+        user=user,
+        passwd=passwd,
+        database=database,
+        charset=charset,
+        port=port
     )
     try:
-        cursor = db.cursor()  
-        sql = "select id, edited_time from newcoder_search where id in ({})".format(",".join([str(x['id']) for x in data]))
+        cursor = db.cursor()
+        sql = "select id, edited_time from newcoder_search where id in ({})".format(
+            ",".join([str(x['id']) for x in data]))
         cursor.execute(sql)
-        an = cursor.fetchall()
-        dic = {x[0] : x[1].strftime("%Y-%m-%d %H:%M:%S") for x in an}
+        exists = cursor.fetchall()
+        dic = {x[0]: x[1].strftime("%Y-%m-%d %H:%M:%S") for x in exists}
 
         insert_data = [[x[k] for k in x] for x in data if x['id'] not in dic]
         update_data = [(x['editTime'], x['id']) for x in data if x['id'] in dic and dic[x['id']] != x['editTime']]
@@ -74,9 +80,11 @@ def upsert_to_db(data, host, user, passwd, database,charset, port):
     except Exception as e:
         print("db error: ", e)
     db.close()
-    return [x for x in data if x['id'] not in dic], [x for x in data if x['id'] in dic and dic[x['id']] != x['editTime']]
+    return [x for x in data if x['id'] not in dic], [x for x in data if
+                                                     x['id'] in dic and dic[x['id']] != x['editTime']]
 
-def table_html_generate(data):
+
+def _table_html_generate(data):
     s = '<table>'
     s += '<tr>' + "\n".join(["<th>" + x + '</th>' for x in data[0]]) + '</tr>'
     for d in data:
@@ -84,43 +92,45 @@ def table_html_generate(data):
     s += '</table>'
     return s
 
+
 def send_email(insert_data, update_data, mail_host, mail_user, mail_pass, sender, receivers):
     msg = ''
     if len(insert_data) > 0:
-        msg += '<h1>insert</h1></br>' + table_html_generate(insert_data) + '</br></br>'
+        msg += '<h1>insert</h1></br>' + _table_html_generate(insert_data) + '</br></br>'
     if len(update_data) > 0:
-        msg += '<h1>update</h1></br>' + table_html_generate(update_data) + '</br></br>'
+        msg += '<h1>update</h1></br>' + _table_html_generate(update_data) + '</br></br>'
     if msg == '':
         msg = '<h1>今日无新增数据</h1></br>'
-        
+
     message = MIMEText(msg, 'html', 'utf-8')
     message['Subject'] = '牛客网{}招聘信息'.format(time.strftime("%Y-%m-%d"))
     message['From'] = sender
     message['To'] = receivers[0]
     try:
         smtpObj = smtplib.SMTP_SSL(mail_host, 465)
-        #smtpObj.connect(mail_host, 465)
+        # smtpObj.connect(mail_host, 465)
         smtpObj.login(mail_user, mail_pass)
         smtpObj.sendmail(
             sender, receivers, message.as_string())
         smtpObj.quit()
         return True
     except smtplib.SMTPException as e:
-        print('email send error: ', e)  
-        return False    
+        print('email send error: ', e)
+        return False
 
-def run(keywords, skip_words, db_config, mail_config = None):
+
+def run(keywords, skip_words, db_config, mail_config=None):
     res = []
     for key in keywords:
         print(key, time.strftime("%Y-%m-%d %H:%M:%S"))
         for i in range(1, 11):
             print(i)
-            a = get_newcoder_page(i, key)
-            b = parse_newcoder_page(a, skip_words)
-            if b == None or len(b) < 1: break
-            res.extend(b)
+            page = get_newcoder_page(i, key, skip_words)
+            if not page:
+                break
+            res.extend(page)
             time.sleep(1)
-    
+
     result, ids = [], set()  # 去重
     for x in res:
         if x['id'] in ids: continue
@@ -132,34 +142,36 @@ def run(keywords, skip_words, db_config, mail_config = None):
     if mail_config:
         send_email(*x, **mail_config)
 
-def main(): 
+
+def main():
     # 指定要过滤的词
-    skip_words=['求捞', '泡池子', '池子了', '池子中', 'offer对比', '总结一下', '给个建议', '开奖群', '没消息', '有消息', '拉垮', '求一个', '求助', '池子的', '决赛圈', 'offer比较', '求捞', '补录面经', '捞捞', '收了我吧', 'offer选择', '有offer了', '想问一下', 'kpi吗', 'kpi面吗', 'kpi面吧']
-    
+    skip_words = ['求捞', '泡池子', '池子了', '池子中', 'offer对比', '总结一下', '给个建议', '开奖群', '没消息', '有消息', '拉垮', '求一个', '求助', '池子的',
+                  '决赛圈', 'offer比较', '求捞', '补录面经', '捞捞', '收了我吧', 'offer选择', '有offer了', '想问一下', 'kpi吗', 'kpi面吗', 'kpi面吧']
+
     # 指定搜索的关键词
     keywords = ['补招', '补录']
-    
-    #配置数据库信息
+
+    # 配置数据库信息
     db_config = {
-        "host" : "localhost", 
-        "user" : "root", 
-        "passwd" : "your password", 
-        "database" : 'your database',
-        "charset" : 'utf8', 
+        "host": "localhost",
+        "user": "root",
+        "passwd": "your password",
+        "database": 'your database',
+        "charset": 'utf8',
         "port": your mysql port
     }
-    
+
     # 配置邮箱信息
     mail_config = {
-        "mail_host" : 'smtp server host', 
-        "mail_user" : 'your user name', 
-        "mail_pass" : 'password',   # 密码(部分邮箱为授权码)
-        "sender" : 'sender email', 
-        "receivers" : ["receivers email"]
+        "mail_host": 'smtp server host',
+        "mail_user": 'your user name',
+        "mail_pass": 'password',  # 密码(部分邮箱为授权码)
+        "sender": 'sender email',
+        "receivers": ["receivers email"]
     }
-    
+
     run(keywords, skip_words, db_config, mail_config)
-    
+
 
 if __name__ == "__main__":
     main()
--- crawler_advanced.py
+++ crawler_advanced.py
@@ -10,7 +10,35 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from email.mime.text import MIMEText
 
 
-def get_newcoder_page(page=1, keyword="校招"):
+def _parse_newcoder_page(data, skip_words, start_date):
+    assert data['success'] == True
+    pattern = re.compile("|".join(skip_words))
+    res = []
+    for x in data['data']['records']:
+        x = x['data']
+        dic = {"user": x['userBrief']['nickname']}
+
+        x = x['contentData'] if 'contentData' in x else x['momentData']
+        dic['title'] = x['title']
+        dic['content'] = x['content']
+        dic['id'] = int(x['id'])
+        dic['url'] = 'https://www.nowcoder.com/discuss/' + str(x['id'])
+        text = str(x['title']) if x['title'] else "" + str(x['content']) if x['content'] else ""
+        if len(skip_words) > 0 and pattern.search(text) != None:  # 关键词正则过滤
+            continue
+
+        createdTime = x['createdAt'] if 'createdAt' in x else x['createTime']
+        dic['createTime'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(createdTime // 1000))
+        dic['editTime'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(x['editTime'] // 1000))
+
+        if dic['editTime'] < start_date:  # 根据时间过滤
+            continue
+        res.append(dic)
+
+    return res
+
+
+def get_newcoder_page(page=1, keyword="校招", skip_words=[], start_date='2023'):
     header = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
         "content-type": "application/json"
@@ -23,34 +51,8 @@ def get_newcoder_page(page=1, keyword="校招"):
         "order": "create"
     }
     x = requests.post('https://gw-c.nowcoder.com/api/sparta/pc/search', data=json.dumps(data), headers=header, )
-    return x.json()
-
-
-def parse_newcoder_page(data, skip_words=[], start_date='2023'):
-    assert data['success'] == True
-    pattern = re.compile("|".join(skip_words), re.I)
-    res = []
-    for x in data['data']['records']:
-        x = x['data']
-        dic = {"user": x['userBrief']['nickname']}
-
-        x = x['contentData'] if 'contentData' in x else x['momentData']
-        dic['title'] = x['title']
-        dic['content'] = x['content']
-        dic['id'] = int(x['id'])
-        dic['url'] = 'https://www.nowcoder.com/discuss/' + str(x['id'])
-
-        if skip_words and pattern.search(x['content'] + x['title'] if x['title'] else "") != None:
-            continue
-
-        createdTime = x['createdAt'] if 'createdAt' in x else x['createTime']
-        dic['createTime'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(createdTime // 1000))
-        dic['editTime'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(x['editTime'] // 1000))
-
-        if dic['editTime'] < start_date: continue
-        res.append(dic)
-
-    return res
+    data = _parse_newcoder_page(x.json(), skip_words, start_date)
+    return data
 
 
 def upsert_to_db(data, host, user, passwd, database, charset, port):
@@ -67,8 +69,8 @@ def upsert_to_db(data, host, user, passwd, database, charset, port):
         sql = "select id, edited_time from newcoder_search where id in ({})".format(
             ",".join([str(x['id']) for x in data]))
         cursor.execute(sql)
-        an = cursor.fetchall()
-        dic = {x[0]: x[1].strftime("%Y-%m-%d %H:%M:%S") for x in an}
+        exists = cursor.fetchall()
+        dic = {x[0]: x[1].strftime("%Y-%m-%d %H:%M:%S") for x in exists}
 
         insert_data = [[x[k] for k in x] for x in data if x['id'] not in dic]
         update_data = [(x['editTime'], x['id']) for x in data if x['id'] in dic and dic[x['id']] != x['editTime']]
@@ -84,7 +86,7 @@ def upsert_to_db(data, host, user, passwd, database, charset, port):
                                                      x['id'] in dic and dic[x['id']] != x['editTime']]
 
 
-def table_html_generate(data):
+def _table_html_generate(data):
     s = '<table>'
     s += '<tr>' + "\n".join(["<th>" + x + '</th>' for x in data[0]]) + '</tr>'
     for d in data:
@@ -96,9 +98,9 @@ def table_html_generate(data):
 def send_email(insert_data, update_data, mail_host, mail_user, mail_pass, sender, receivers):
     msg = ''
     if len(insert_data) > 0:
-        msg += '<h1>insert</h1></br>' + table_html_generate(insert_data) + '</br></br>'
+        msg += '<h1>insert</h1></br>' + _table_html_generate(insert_data) + '</br></br>'
     if len(update_data) > 0:
-        msg += '<h1>update</h1></br>' + table_html_generate(update_data) + '</br></br>'
+        msg += '<h1>update</h1></br>' + _table_html_generate(update_data) + '</br></br>'
     if msg == '':
         msg = '<h1>今日无新增数据</h1></br>'
 
@@ -119,20 +121,22 @@ def send_email(insert_data, update_data, mail_host, mail_user, mail_pass, sender
         return False
 
 
-def batch_generate(texts, tokenizer, model, id2label={0: '招聘信息', 1: '经验贴', 2: '求助贴'}, max_length=128):
+def _batch_generate(texts, model, tokenizer, id2label={0: '招聘信息', 1: '经验贴', 2: '求助贴'}, max_length=128):
     inputs = tokenizer(texts, return_tensors="pt", max_length=128, padding=True, truncation=True)
     outputs = model(**inputs).logits.argmax(-1).tolist()
     return [id2label[x] for x in outputs]
 
 
-def model_predict(text_list, model_name="roberta4h512", batch_size=4):
+def model_predict(text_list, model=None, tokenizer=None, model_name="roberta4h512", batch_size=4):
     if not text_list: return []
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    if not model:
+        model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    if not tokenizer:
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
     model.eval()
     result, start = [], 0
     while (start < len(text_list)):
-        result.extend(batch_generate(text_list[start: start + batch_size], tokenizer, model))
+        result.extend(_batch_generate(text_list[start: start + batch_size], model, tokenizer))
         start += batch_size
     return result
 
@@ -148,15 +152,15 @@ def get_config(SERVER_ADDRESSES, NAMESPACE, GROUP):
     return keywords, skip_words, db_config, mail_config
 
 
-def filter(data, exists):
-    # 模型过滤，根据内容去重
+def filter(data, unique_content, model=None, tokenizer=None):
+    # 模型过滤，根据页面内容去重
     labels = model_predict([(str(x['title']) if x['title'] else "") + "\t" +
-                            (str(x['content']) if x['content'] else "") for x in data])
+                            (str(x['content']) if x['content'] else "") for x in data], model, tokenizer)
     result = []
     for i, x in enumerate(data):
-        if x['content'] in exists or labels[i] != "招聘信息":
+        if x['content'] in unique_content or labels[i] != "招聘信息":
             continue
-        exists.add(x['content'])
+        unique_content.add(x['content'])
         result.append(x)
     return result
 
@@ -165,14 +169,14 @@ def run(keywords, skip_words, db_config, mail_config=None):
     res = []
     for key in keywords:
         print(key, time.strftime("%Y-%m-%d %H:%M:%S"))
-        for i in range(1, 31):
+        for i in range(1, 21):
             print(i)
-            a = get_newcoder_page(i, key)
-            b = parse_newcoder_page(a, skip_words,
-                                    start_date=time.strftime("%Y-%m-%d",
-                                                             time.localtime(time.time() - 15 * 24 * 60 * 60)))
-            if b == None or len(b) < 1: break
-            res.extend(b)
+            page = get_newcoder_page(i, key, skip_words,
+                                     start_date=time.strftime("%Y-%m-%d",
+                                                              time.localtime(time.time() - 15 * 24 * 60 * 60)))
+            if not page:
+                break
+            res.extend(page)
             time.sleep(1)
 
     res.sort(key=lambda x: len(x['content']))
@@ -185,15 +189,18 @@ def run(keywords, skip_words, db_config, mail_config=None):
 
     print("total num: ", len(result))
     # print(result)
-    x = upsert_to_db(result, **db_config)  # insert_data, update_data
-    exists = set()
+    insert_data, update_data = upsert_to_db(result, **db_config)  # insert_data, update_data
+
     if mail_config:
-        send_email(filter(x[0], exists), filter(x[1], exists), **mail_config)
+        unique_content, shared_model, shared_tokenizer = set(), None, None
+        insert_data = filter(insert_data, unique_content, shared_model, shared_tokenizer)
+        update_data = filter(update_data, unique_content, shared_model, shared_tokenizer)
+        send_email(insert_data, update_data, **mail_config)
 
 
 def main():
-    SERVER_ADDRESSES = "localhost"
-    NAMESPACE = "your space"
+    SERVER_ADDRESSES = "ip:port"
+    NAMESPACE = "your namespace"
     GROUP = "your group"
 
     run(*get_config(SERVER_ADDRESSES, NAMESPACE, GROUP))
